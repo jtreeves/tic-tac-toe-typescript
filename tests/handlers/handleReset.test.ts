cec2deea01ac860f5c7188f6513705b8a915544e
jtreeves <jackson.t.reeves@gmail.com>
2022-10-24 19:40:20 -0400

    [tests] Add cases for handleReset

---
+++ tests/handlers/handleReset.test.ts
@@ -0,0 +1,36 @@
+import handleReset from '../../src/handlers/handleReset'
+
+describe('handleReset handler', () => {
+    beforeEach(() => {
+        localStorage.setItem('points', '1,0,1,0,-1,0,1,0,-1')
+        localStorage.setItem('turn', '-1')
+        localStorage.setItem('winner', 'false')
+        localStorage.setItem('tie', 'false')
+        document.body.innerHTML = `
+            <h2>Some reminder</h2>
+            <p>Some message</p>
+            <section>Some contents</section>
+            <button>Reset</button>
+        `
+    })
+
+    it('should create screen with no h2 or section tags', () => {
+        handleReset()
+        const h2: HTMLElement | null = document.querySelector('h2')
+        const section: HTMLElement | null = document.querySelector('section')
+        expect(h2).toBe(null)
+        expect(section).toBe(null)
+    })
+
+    it('should set points to array of all zeroes', () => {
+        handleReset()
+        const points: string | null = localStorage.getItem('points')
+        expect(points).toBe('0,0,0,0,0,0,0,0,0')
+    })
+
+    it('should set turn to 1', () => {
+        handleReset()
+        const turn: string | null = localStorage.getItem('turn')
+        expect(turn).toBe('1')
+    })
+})
--- tests/index.ts
+++ tests/index.ts
@@ -1,1 +1,1 @@
-// Ran 103 passing tests in 4.035s on 10/24/22
+// Ran 106 passing tests in 3.751s on 10/24/22
