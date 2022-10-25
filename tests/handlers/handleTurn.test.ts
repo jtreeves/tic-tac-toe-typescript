e1e07ddbafabd3c9526a24787b0bdf3ebca1b3b1
jtreeves <jackson.t.reeves@gmail.com>
2022-10-24 22:43:23 -0400

    [tests] Add more cases for handleTurn

--- tests/handlers/handleTurn.test.ts
+++ tests/handlers/handleTurn.test.ts
@@ -14,4 +14,39 @@ describe('handleTurn handler', () => {
         const updatedArticle: HTMLElement = document.querySelector('article') as HTMLElement
         expect(updatedArticle.textContent).toBe('X')
     })
+    
+    it('should update textContent of target on event to O when player and turn are both -1', () => {
+        localStorage.setItem('player', '-1')
+        localStorage.setItem('turn', '-1')
+        const article: HTMLElement = document.querySelector('article') as HTMLElement
+        article.addEventListener('click', handleTurn)
+        article.click()
+        const updatedArticle: HTMLElement = document.querySelector('article') as HTMLElement
+        expect(updatedArticle.textContent).toBe('O')
+    })
+    
+    it('should not update textContent of target on event when player and turn are not equal', () => {
+        localStorage.setItem('player', '1')
+        localStorage.setItem('turn', '-1')
+        const article: HTMLElement = document.querySelector('article') as HTMLElement
+        const articleContent: string | null = article.textContent
+        article.addEventListener('click', handleTurn)
+        article.click()
+        const updatedArticle: HTMLElement = document.querySelector('article') as HTMLElement
+        const updatedArticleContent: string | null = updatedArticle.textContent
+        expect(articleContent).toBe(updatedArticleContent)
+    })
+    
+    it('should not update textContent of target on event when content already exists', () => {
+        localStorage.setItem('player', '1')
+        localStorage.setItem('turn', '1')
+        document.body.innerHTML = `<article id='square-5'>O</article>`
+        const article: HTMLElement = document.querySelector('article') as HTMLElement
+        const articleContent: string | null = article.textContent
+        article.addEventListener('click', handleTurn)
+        article.click()
+        const updatedArticle: HTMLElement = document.querySelector('article') as HTMLElement
+        const updatedArticleContent: string | null = updatedArticle.textContent
+        expect(articleContent).toBe(updatedArticleContent)
+    })
 })
--- tests/index.ts
+++ tests/index.ts
@@ -1,1 +1,1 @@
-// Ran 108 passing tests in 3.57s on 10/24/22
+// Ran 111 passing tests in 3.628s on 10/24/22
