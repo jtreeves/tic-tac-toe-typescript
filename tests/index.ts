affce22a720ef41924bac88cba678a7ac4f1519d
jtreeves <jackson.t.reeves@gmail.com>
2022-10-29 13:49:27 -0400

    [tests] Mock out getValue in accessor case

--- tests/index.ts
+++ tests/index.ts
@@ -1,1 +1,1 @@
-// Ran 133 passing tests in 3.762s on 10/29/22
+// Ran 134 passing tests in 3.753s on 10/29/22
--- tests/unit/accessors/getPlayer.test.ts
+++ tests/unit/accessors/getPlayer.test.ts
@@ -1,20 +1,28 @@
+import * as getValueModule from '../../../src/accessors/getValue'
 import getPlayer from '../../../src/accessors/getPlayer'
 
 describe('getPlayer accessor', () => {
-    const testKey: string = 'player'
-    const testValue: string = '1'
-
-    beforeEach(() => {
-        localStorage.setItem(testKey, testValue)
-    })
-
     it('should return a number', () => {
+        const spy = jest.spyOn(getValueModule, 'default')
+        spy.mockReturnValue('1')
         const result: any = getPlayer()
         expect(typeof result).toBe('number')
+        spy.mockRestore()
     })
-
-    it('should return a value matching the one initially set, but as a numerical coercion', () => {
+    
+    it('should call getValue once', () => {
+        const spy = jest.spyOn(getValueModule, 'default')
+        getPlayer()
+        expect(spy).toBeCalledTimes(1)
+        spy.mockRestore()
+    })
+    
+    it('should return numerical version of string returned by getValue', () => {
+        const returnedString: string = '1'
+        const spy = jest.spyOn(getValueModule, 'default')
+        spy.mockReturnValue(returnedString)
         const result: number = getPlayer()
-        expect(result).toBe(Number(testValue))
+        expect(result).toBe(Number(returnedString))
+        spy.mockRestore()
     })
 })
