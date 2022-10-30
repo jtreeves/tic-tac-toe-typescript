e1ea13812f2b52d6f8f2f7ffadf4829fd35abe8a
jtreeves <jackson.t.reeves@gmail.com>
2022-10-30 16:09:20 -0400

    [tests/integration] Add case to confirm state reset after clicking reset button

--- tests/integration/clickToUpdate.test.ts
+++ tests/integration/clickToUpdate.test.ts
@@ -1,6 +1,10 @@
+import getPoints from '../../src/accessors/getPoints'
+import getWinner from '../../src/accessors/getWinner'
 import getPlayer from '../../src/accessors/getPlayer'
 import setInitialStates from '../../src/storers/setInitialStates'
 import setPlayer from '../../src/storers/setPlayer'
+import setWinner from '../../src/storers/setWinner'
+import setPoints from '../../src/storers/setPoints'
 import setTurn from '../../src/storers/setTurn'
 import createInitialScreen from '../../src/generators/createInitialScreen'
 import updateScreenWithOptions from '../../src/mutators/updateScreenWithOptions'
@@ -57,6 +61,21 @@ describe('click to update integration', () => {
         expect(resetButton?.textContent).toBe('Reset')
         expect(optionButtons[0].textContent).toBe('Play X')
         expect(optionButtons[1].textContent).toBe('Play O')
+    })
+
+    it('should reset points to array of zeroes and winner to false if click reset button on game screen', () => {
+        document.body.innerHTML = ``
+        createInitialScreen()
+        updateScreenWithOptions()
+        updateScreenWithGame()
+        setPoints([1, 1, 1, 0, 0, 0, 0, 0, 0])
+        setWinner(true)
+        const resetButton: HTMLElement | null = document.querySelector('button')
+        resetButton?.click()
+        const points: number[] = getPoints()
+        const winner: boolean = getWinner()
+        expect(points).toStrictEqual(Array(9).fill(0))
+        expect(winner).toBe(false)
     })
 
     it('should change first cell content from empty to X if click first cell on game screen and player set to 1', () => {
